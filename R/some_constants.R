2c17321a8d21eab3954e3e6584855cf2b4f4c997
Cruz <cruz.julian.mauricio@gmail.com>
2015-08-05 15:40:35 -0500

    Empty symbol implementation.

--- R/some_constants.R
+++ R/some_constants.R
@@ -1,11 +1,13 @@
 
 ## SET SOME CONSTANTS FOR THE LOGICAL LANGUAGE
 
+empty <- quote(expr=)
+
 
 f1 <- "formula1"
 f2 <- "formula2"
 f3 <- "formula3"
-f4 <- "formula4"
+f4 <- "ModLogDF"
 
 Error.1 <- "There are non-logical characters un the formula string."
 Error.2 <- "Unnapropiated object type"
@@ -18,13 +20,13 @@ parentheses <- c(open_parentheses, close_parentheses)
 unary_symbols <- c("¬", "0")
 binary_symbols <- c("|", "&", ">")
 
-object_types <- factor(c("0", "unary", "binary", "open", "close", "term", "X"), 
+object_types <- factor(c("0", "unary", "binary", "open", "close", "term", "X"),
                        levels = c("0", "unary", "binary", "open", "close", "term", "X"))
 
 
-walk_matrix <- 
+walk_matrix <-
   matrix(
-    c(0, 1, 0, 1, 0, 0, 0,
+    c(0, 1, 0, 1, 0, 1, 0,
       0, 0, 0, 1, 0, 0, 0,
       0, 0, 0, 1, 0, 0, 0,
       0, 1, 0, 1, 0, 1, 0,
@@ -33,7 +35,7 @@ walk_matrix <-
       0, 0, 0, 0, 0, 0, 1
     ),
     byrow = TRUE,
-    ncol = 7, 
+    ncol = 7,
     dimnames = list(
       object_types,
       object_types
