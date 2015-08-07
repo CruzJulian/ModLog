8803a2fac8e9441ba1f271c5374b4964e5b33a08
Cruz <cruz.julian.mauricio@gmail.com>
2015-08-06 20:35:54 -0500

    Setting symbols and names for the branches and worlds.

--- R/some_constants.R
+++ R/some_constants.R
@@ -10,13 +10,26 @@ f4 <- "ModLogDF"
 Error.1 <- "There are non-logical characters un the formula string."
 Error.2 <- "Unnapropiated object type"
 Error.3 <- "This is not a well formed formula"
-Error.4 <- "There is something wron with the classes."
+Error.4 <- "There is something wrong with the classes."
+Error.5 <- "This is a term. It can not be negated forward."
+
+name_branches = "branche"
+name_worlds = "world"
+names_format <- c(pre = "$", middle = "_", post = "$")
 
 open_parentheses <- c("(", "[")
 close_parentheses <- c(")", "]")
 parentheses <- c(open_parentheses, close_parentheses)
-unary_symbols <- c("¬", "0")
-binary_symbols <- c("|", "&", ">")
+unary_symbols <- c(NOT = "¬", POSS = "0")
+binary_symbols <- c(OR = "+|", AND = "&", THEN = ">")
+
+symbols_ <- data.frame(
+  symbols = c(unary_symbols, binary_symbols),
+  numeric = 1:length(c(unary_symbols, binary_symbols)),
+  names = names(c(unary_symbols, binary_symbols)),
+  stringsAsFactors = FALSE
+)
+
 
 object_types <- factor(c("0", "unary", "binary", "open", "close", "term", "X"),
                        levels = c("0", "unary", "binary", "open", "close", "term", "X"))
